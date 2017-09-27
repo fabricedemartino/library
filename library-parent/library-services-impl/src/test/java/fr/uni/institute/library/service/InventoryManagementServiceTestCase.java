4865916a3d9810fd9cb6421110425250e8c7c8b5
fabricedemartino <fabrice.demartino@sfr.com>
2017-09-27 16:23:33 +0200

    Initialisation de projet library

---
+++ library-parent/library-services-impl/src/test/java/fr/uni/institute/library/service/InventoryManagementServiceTestCase.java
@@ -0,0 +1,94 @@
+package fr.uni.institute.library.service;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import fr.uni.institute.library.business.inventory.Category;
+import fr.uni.institute.library.dao.jdbc.CategoryDaoJdbc;
+import fr.uni.institute.library.service.impl.InventoryManagementServiceImpl;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class InventoryManagementServiceTestCase {
+
+	private static InventoryManagementService service;
+	private static int nombreCategories;
+	private static Category categorieAttendue; 
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		System.out.println("-- Initialisation setUpClass de jeu de tests");
+		System.out.println("Initialisation de jeu de tests");
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/uni_library_db?autoReconnect=true", "root", "admin");
+		service = new InventoryManagementServiceImpl();		
+		service.setCategoryDao(new CategoryDaoJdbc(connection));
+		nombreCategories = 9;
+		//
+		categorieAttendue = new Category(1, "Music");
+	}
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("liberation de jeu de tests");
+	}
+	
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		System.out.println("-- liberation  tearDownClass de jeu de tests");
+	}
+
+	@Test
+	public final void test1GetCategoryById() {
+		System.out.println("testGetCategoryById");
+		
+		try{
+			Category resultat = service.getCategoryById(categorieAttendue.getId());
+			assertNotNull(resultat);
+			assertEquals(resultat.getName(), categorieAttendue.getName());	
+		}catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	
+	@Test
+	public final void test2GetCategoryAll() {
+		System.out.println("testGetCategoryAll");
+		try{
+			Collection categories = service.getAllCategories() ;
+			assertNotNull(categories);
+			assertEquals(nombreCategories, categories.size());	
+		}catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+		
+	}
+	
+	
+	@Test(expected=ServiceException.class)
+	public final void test3GetCategoryAllWithException() throws ServiceException {
+		System.out.println("test3GetCategoryAllWithException");
+		throw new ServiceException();
+		
+	}
+
+}
